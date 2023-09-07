0bdd7be2fc45de27862a113635de6d1d8edcb8f2
paskanov <v.paskanov@gmail.com>
2023-09-07 16:44:21 +0300

    Refactoring with search Players.tsx

--- src/features/players/Players.tsx
+++ src/features/players/Players.tsx
@@ -4,9 +4,15 @@ import s from "../players/styles.module.scss"
 
 export const Players = () => {
 
+
+    const onSearchPlayers = () => {
+        return alert("Successfully search")
+    }
+
+
     return <div className={s.players}>
         <div><input placeholder={"Search player"}/>
-            <Button variant={'primary'}/>
+            <Button onClick={onSearchPlayers} className={s.buttonSearchPlayer} variant={'primary'}>Search</Button>
         </div>
         <div><Player/></div>
     </div>
