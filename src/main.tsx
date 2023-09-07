5f78ea58651d60e823aa5453f4baa8d0d637749b
paskanov <v.paskanov@gmail.com>
2023-09-07 12:54:34 +0300

    Add routes and changed some css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './app/App.tsx'
 import './index.css'
 import {Header} from "./features/header/Header";
+import {Players} from "./features/players/Players";
+import {createBrowserRouter} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>Hello world!</div>,
+    },
+    {
+        path: '/login',
+        // element: <Login/>,
+
+    },
+    {
+        path: '/players',
+        element: <Players/>
+    },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
