dd04ff85bff05e16a6125a4be8bbdf195e613ec5
paskanov <v.paskanov@gmail.com>
2023-09-07 15:57:44 +0300

    Some refactoring routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM, {createRoot} from 'react-dom/client'
 import App from './app/App.tsx'
 import './index.css'
 import {Header} from "./features/header/Header";
 import {Players} from "./features/players/Players";
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Provider} from "react-redux";
 
 
 const router = createBrowserRouter([
@@ -23,10 +24,17 @@ const router = createBrowserRouter([
     },
 ])
 
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+      <Provider>
+
       <Header/>
     <App />
+      </Provider>
+      <RouterProvider router={router}/>
   </React.StrictMode>,
 )
