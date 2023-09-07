c730cc5d4adf2bc14b9dee9df28ce535c155473e
paskanov <v.paskanov@gmail.com>
2023-09-07 12:29:28 +0300

    Some changed with structure of files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/public/vite.svg'
 import './App.css'
-import {Players} from "./features/players/Players";
+import {Players} from "../features/players/Players";
 
 function App() {
     // const [player, setPlayer] = useState(0)
--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,7 +1,12 @@
 import {Button} from "../../components/ui/button";
+import s from "../header/styles.module.scss"
 
 export const Header = () => {
-    return <div>
-        <Button></Button>
+    return (
+    <header className={s.header}>
+    <div>
+        <Button variant={"primary"}>Sign In</Button>
     </div>
+    </header>
+    )
 }
