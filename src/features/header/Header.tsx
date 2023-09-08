f3f22d576249a2b404d7d3ffcb8bf953436c9972
paskanov <v.paskanov@gmail.com>
2023-09-08 11:33:31 +0300

    Some refactoring with header

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,9 +1,11 @@
 import {Button} from "../../components/ui/button";
 import s from "../header/styles.module.scss"
+import {Players} from "../players/Players";
 
 export const Header = () => {
     return (
     <header className={s.header}>
+
     <div className={s.button}>
         <Button variant={"primary"}>Sign In</Button>
     </div>
