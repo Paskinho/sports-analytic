1f0cf0f9edf9e979f419ce554a4017f4cf57ae8a
paskanov <v.paskanov@gmail.com>
2023-09-08 11:56:20 +0300

    Create slice for app

---
+++ src/app/app.slice.ts
@@ -0,0 +1,20 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+const appInitialState = {
+    error: null as string | null,
+    isLoading: false,
+    isAppInitialized: false,
+
+}
+
+type InitialStateType = typeof appInitialState
+
+const slice = createSlice({
+    name: 'app',
+    initialState: appInitialState,
+    reducers: {
+        setIsLoading: (state, action: PayloadAction<{isLoading: boolean}>)=> {
+            state.isLoading = action.payload.isLoading
+        }
+    }
+})
