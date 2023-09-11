060975812d07c790ccce23228a9cad1889a44f66
paskanov <v.paskanov@gmail.com>
2023-09-11 12:55:53 +0300

    Add typography component in Header

--- src/components/ui/typography/typography.tsx
+++ src/components/ui/typography/typography.tsx
@@ -1,6 +1,6 @@
 import { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
 
-import s from "./typography.module.scss";
+import s from "../typography/typography.module.css";
 
 export type TypographyProps = {
     children: ReactNode;
--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,10 +1,12 @@
 import {Button} from "../../components/ui/button";
 import s from "../header/styles.module.scss"
 import {Players} from "../players/Players";
+import {Typography} from "../../components/ui/typography";
 
 export const Header = () => {
     return (
     <header className={s.header}>
+        <Typography variant={'link1'}>Home</Typography>
     <div className={s.button}>
         <Button variant={"primary"}>Sign In</Button>
     </div>
