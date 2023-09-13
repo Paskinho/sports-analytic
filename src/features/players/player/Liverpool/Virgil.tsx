194a22d2812b8aaed7e401cffffac0beaaad3ea9
paskanov <v.paskanov@gmail.com>
2023-09-13 17:09:08 +0300

    Some refactoring in styles and in code

--- src/features/players/player/Liverpool/Virgil.tsx
+++ src/features/players/player/Liverpool/Virgil.tsx
@@ -1,6 +1,9 @@
+import s from '../../../../common/styles/player.module.scss'
+
+
 export const Virgil = () => {
     return <div>
-        Virgil
-        <img src={"https://backend.liverpoolfc.com/sites/default/files/styles/xl/public/2023-07/virgil-van-dijk-statement-cover-.webp?itok=9Oyo-aSV"}/>
+        <div>Virgil</div>
+        <img className={s.img} src={"https://backend.liverpoolfc.com/sites/default/files/styles/xl/public/2023-07/virgil-van-dijk-statement-cover-.webp?itok=9Oyo-aSV"}/>
     </div>
 }
