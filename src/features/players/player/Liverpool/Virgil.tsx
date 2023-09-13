085168fceebfd87df654c2bf9ad688b8cae24784
paskanov <v.paskanov@gmail.com>
2023-09-13 17:12:52 +0300

    change some styles

--- src/features/players/player/Liverpool/Virgil.tsx
+++ src/features/players/player/Liverpool/Virgil.tsx
@@ -2,8 +2,8 @@ import s from '../../../../common/styles/player.module.scss'
 
 
 export const Virgil = () => {
-    return <div>
-        <div>Virgil</div>
+    return <div className={s.page}>
+        <div>#4 Virgil Van Dijk</div>
         <img className={s.img} src={"https://backend.liverpoolfc.com/sites/default/files/styles/xl/public/2023-07/virgil-van-dijk-statement-cover-.webp?itok=9Oyo-aSV"}/>
     </div>
 }
