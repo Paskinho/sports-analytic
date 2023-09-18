d4e695cf3619d69c78a0bdb75f449aa5bae64345
paskanov <v.paskanov@gmail.com>
2023-09-18 15:46:31 +0300

    Add styled components

--- src/features/players/player/Liverpool/Virgil.tsx
+++ src/features/players/player/Liverpool/Virgil.tsx
@@ -1,10 +1,13 @@
 import s from '../../../../common/styles/player.module.scss'
+import styled from "styled-components";
 
 
 export const Virgil = () => {
     return <div className={s.page}>
         <div>#4 Virgil Van Dijk</div>
+
         <img className={s.img} src={"https://backend.liverpoolfc.com/sites/default/files/styles/xl/public/2023-07/virgil-van-dijk-statement-cover-.webp?itok=9Oyo-aSV"}/>
     </div>
 }
 
+const Img = styled.image {}
