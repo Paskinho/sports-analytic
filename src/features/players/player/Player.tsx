f9466f7060a8f7ed8a1bf047d8ff0d6d9e5f7b9e
paskanov <v.paskanov@gmail.com>
2023-09-19 16:09:04 +0300

    Add some styled components

--- src/features/players/player/Player.tsx
+++ src/features/players/player/Player.tsx
@@ -1,6 +1,7 @@
 import {Typography} from "../../../components/ui/typography";
 import {useNavigate} from "react-router-dom";
 import {PlayerType} from "./player.slice";
+import styled from "styled-components";
 
 
 
@@ -13,26 +14,38 @@ export const Player = (props: PlayerType) => {
         return navigate("/Virgil")
     }
 
-    return <table>
-        <td>
+    return <StyledTable>
+        <StyledTd>
         <div>
             <Typography variant={'caption'} onClick={Virg}>Virgil Van Dijk</Typography>
             </div>
         <div><Typography variant={'caption'} onClick={Virg}>Mohamed Salah</Typography></div>
         <div><Typography variant={'caption'} onClick={Virg}>Darwin Nunez</Typography></div>
         <div><Typography variant={'caption'} onClick={Virg}>Alisson Becker</Typography></div>
-        </td>
-        <td>
+        </StyledTd>
+        <StyledTd>
             <Typography variant={'caption'}>Netherlands</Typography>
             <Typography variant={'caption'}>Egypt</Typography>
             <Typography variant={'caption'}>Uruguay</Typography>
             <Typography variant={'caption'}>Brazil</Typography>
-        </td>
-        <td>
+        </StyledTd>
+        <StyledTd>
             <Typography variant={'caption'}>Liverpool</Typography>
             <Typography variant={'caption'}>Liverpool</Typography>
             <Typography variant={'caption'}>Liverpool</Typography>
             <Typography variant={'caption'}>Liverpool</Typography>
-        </td>
-    </table>
+        </StyledTd>
+    </StyledTable>
 }
+
+const StyledTd = styled.td `
+border-color: #1a1a1a;
+  //border-width: 50px;
+`
+
+
+const StyledTable = styled.table `
+border-color: #535bf2;
+  border-width: 50px;
+  background-color: beige;
+`
