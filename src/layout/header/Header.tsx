2ee46f5b8b1cf33d0dc81127c1b9f778c5816623
paskanov <v.paskanov@gmail.com>
2023-09-19 12:43:47 +0300

    Add Icon and some refactoring

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -35,14 +35,15 @@ export const Header = () => {
     return (
         <StyledHeader>
 
-            <nav className={s.typography}>
+            <StyledNav>
                 <Logo/>
                 <Typography variant={'subtitle1'} color={'inherit'} onClick={navToHome}>Home</Typography>
                 <Typography variant={'subtitle1'} color={'inherit'} onClick={navToPlayers}>Players</Typography>
                 <Typography variant={'subtitle1'} color={'inherit'} onClick={navToTeams}>Teams</Typography>
                 <Typography variant={'subtitle1'} color={'inherit'} onClick={navToStatistics}>Statistics</Typography>
-                <Typography variant={'subtitle1'} color={'inherit'} onClick={navToSubscription}>Subscription</Typography>
-            </nav>
+                <Typography variant={'subtitle1'} color={'inherit'}
+                            onClick={navToSubscription}>Subscription</Typography>
+            </StyledNav>
             <div className={s.button}>
                 <Button variant={"primary"}>Sign In</Button>
             </div>
@@ -51,7 +52,7 @@ export const Header = () => {
 }
 
 
-const StyledHeader  = styled.header `
+const StyledHeader = styled.header`
   height: 70px;
   box-shadow: 1px 2px 1px 1px lightgray;
   background-color: rgb(255, 44, 0);
@@ -61,3 +62,11 @@ const StyledHeader  = styled.header `
   justify-content: space-between;
   align-items: center;
   padding-right: 50px;`
+
+
+const StyledNav = styled.nav `
+  display: flex;
+  width: 450px;
+  justify-content: space-around;
+  align-content: start;
+`
