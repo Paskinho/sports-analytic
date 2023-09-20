a8edba3a3c6c695e395ef6eb2fa0f17bc8ee5a73
paskanov <v.paskanov@gmail.com>
2023-09-20 16:33:10 +0300

    Create Flex wrapper component

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,19 +1,22 @@
 import styled from "styled-components";
 import photo from "../../../assets/images/Ronaldo News.jpeg"
+import {FlexWrapper} from "../../../components/FlexWrapper";
 
 export const Main = () => {
     return (
         <div>
+            <FlexWrapper align={"center"} justify={"space-around"}>
             <div>
                 <h1>FOOTBALL NEWS </h1>
             </div>
             <Photo src={photo} alt='PSV and Philips'/>
+            </FlexWrapper>
         </div>
     );
 };
 
 
 const Photo = styled.img`
-  width: 100px;
-  height: 100px
+  width: 300px;
+  height: 350px
 `
