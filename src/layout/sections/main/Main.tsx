1943455d2b2f45d2cc96464668c5c2d1bc3c566f
paskanov <v.paskanov@gmail.com>
2023-09-20 16:37:29 +0300

    Some changed css

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,19 +4,28 @@ import {FlexWrapper} from "../../../components/FlexWrapper";
 
 export const Main = () => {
     return (
-        <div>
+        <StyledMain>
             <FlexWrapper align={"center"} justify={"space-around"}>
             <div>
-                <h1>FOOTBALL NEWS </h1>
+                <MainTitle>FOOTBALL NEWS </MainTitle>
             </div>
             <Photo src={photo} alt='PSV and Philips'/>
             </FlexWrapper>
-        </div>
+        </StyledMain>
     );
 };
+
+const StyledMain = styled.div`
+  min-height: 100vh;
+  background-color: #f8eded;
+`
 
 
 const Photo = styled.img`
   width: 300px;
   height: 350px
+`
+
+const MainTitle = styled.h1`
+    
 `
