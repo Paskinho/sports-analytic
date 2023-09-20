a15527f1db1e301c9f3a080b26ea1d0bb13d2031
paskanov <v.paskanov@gmail.com>
2023-09-20 12:33:36 +0300

    add display errors in login form

--- src/features/auth/LoginForm/login-form.tsx
+++ src/features/auth/LoginForm/login-form.tsx
@@ -12,7 +12,11 @@ type FormValues = {
 
 export const LoginForm = () => {
 
-    const {control,register, handleSubmit} = useForm<FormValues>()
+    const {control,register, handleSubmit,
+    formState: {errors}
+    } = useForm<FormValues>()
+
+    console.log(errors)
 
     const onSubmit = (data: FormValues) => {
         console.log(data)
@@ -32,11 +36,15 @@ export const LoginForm = () => {
                 {required: 'Email is required',
                 pattern: {value: `emailRegex`, message: 'Invalid email'} // уточнить по emailRegex
                 }
-            )}  label='email'/>
+            )}  label='email'
+            errorMessage={errors.email?.message}
+            />
             <TextField {...register("password",{
                 required: 'Password is required',
                 minLength: {value: 3, message: "Password has to be at least 3 characters long"}
-            })}  label='password'/>
+            })}  label='password'
+                       errorMessage={errors.password?.message}
+            />
             {/*<Checkbox onCheckedChange={onChange} checked={value} label={'remember me'}  />*/}
             <Button type={"submit"}>Login</Button>
         </form>
