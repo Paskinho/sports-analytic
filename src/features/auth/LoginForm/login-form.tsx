111e0d1c9ca61aa77ea316e559915a2980a878a0
paskanov <v.paskanov@gmail.com>
2023-09-20 12:45:48 +0300

    Add zod validation to login-form.tsx

--- src/features/auth/LoginForm/login-form.tsx
+++ src/features/auth/LoginForm/login-form.tsx
@@ -3,18 +3,28 @@ import {Button} from "../../../components/ui/button";
 import {TextField} from "../../../components/ui/text-field";
 import {useController, useForm} from "react-hook-form";
 import {Checkbox} from "../../../components/ui/checkbox/checkbox";
+import {z} from "zod";
+import { zodResolver } from '@hookform/resolvers/zod'
 
-type FormValues = {
-    email: string,
-    password: string
-}
+
+
+const loginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(3),
+    rememberMe: z.boolean().default(false)
+})
+
+type FormValues = z.infer<typeof loginSchema>
+
 
 
 export const LoginForm = () => {
 
     const {control,register, handleSubmit,
     formState: {errors}
-    } = useForm<FormValues>()
+    } = useForm<FormValues>({
+        resolver: zodResolver(loginSchema)
+    })
 
     console.log(errors)
 
@@ -32,17 +42,10 @@ export const LoginForm = () => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <TextField {...register("email",
-                {required: 'Email is required',
-                pattern: {value: `emailRegex`, message: 'Invalid email'} // уточнить по emailRegex
-                }
-            )}  label='email'
+            <TextField {...register("email")}  label='email'
             errorMessage={errors.email?.message}
             />
-            <TextField {...register("password",{
-                required: 'Password is required',
-                minLength: {value: 3, message: "Password has to be at least 3 characters long"}
-            })}  label='password'
+            <TextField {...register("password")}  label='password'
                        errorMessage={errors.password?.message}
             />
             {/*<Checkbox onCheckedChange={onChange} checked={value} label={'remember me'}  />*/}
