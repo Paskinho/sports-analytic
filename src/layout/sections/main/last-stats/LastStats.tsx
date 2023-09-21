8a21ad01139361b8f40fcf780b0bae36cc21ddc5
paskanov <v.paskanov@gmail.com>
2023-09-21 12:49:55 +0300

    Refactoring in stats

--- src/layout/sections/main/last-stats/LastStats.tsx
+++ src/layout/sections/main/last-stats/LastStats.tsx
@@ -1,38 +1,14 @@
 import styled from "styled-components";
 import {FlexWrapper} from "../../../../components/FlexWrapper";
-import {Icon} from "../../../../assets/icons/Icon";
-import {Typography} from "../../../../components/ui/typography";
 import {StatsTitle} from "../../../../components/StatsTitle";
+import {LastStat} from "./last-stat/LastStat";
 
 export const LastStats = () => {
     return (
         <StyledStats>
             <StatsTitle>
                 <FlexWrapper>
-                    <Stat>
-                        <StatTitle>Shots</StatTitle>
-                        <StatDescription>
-                            <table>
-                                <td>
-                                    <th>Pos.</th>
-                                    <Typography>1</Typography>
-                                </td>
-                                <td>
-                                    <th>Player</th>
-                                    <Typography>Salah</Typography>
-                                </td>
-                                <td>
-                                    <th>Shots</th>
-                                    <Typography>24(9 in target)</Typography>
-                                </td>
-                                <td>
-                                    <th>Conversation</th>
-                                    <Typography>37,5%</Typography>
-                                </td>
-                            </table>
-                        </StatDescription>
-                    </Stat>
-
+                    <LastStat/>
                 </FlexWrapper>
             </StatsTitle>
         </StyledStats>
--- src/layout/sections/main/last-stats/last-stat/LastStat.tsx
+++ src/layout/sections/main/last-stats/last-stat/LastStat.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import {Typography} from "../../../../../components/ui/typography";
+import styled from "styled-components";
 
 export const LastStat = () => {
     return (
         <div>
-
+            <StatTitle>Shots</StatTitle>
+            <StatDescription>
+                <table>
+                    <td>
+                        <th>Pos.</th>
+                        <Typography>1</Typography>
+                    </td>
+                    <td>
+                        <th>Player</th>
+                        <Typography>Salah</Typography>
+                    </td>
+                    <td>
+                        <th>Shots</th>
+                        <Typography>24(9 in target)</Typography>
+                    </td>
+                    <td>
+                        <th>Conversation</th>
+                        <Typography>37,5%</Typography>
+                    </td>
+                </table>
+            </StatDescription>
         </div>
     );
 };
+
+
+const StatTitle = styled.h3`
+
+`
+
+const StatDescription = styled.div `
+`
