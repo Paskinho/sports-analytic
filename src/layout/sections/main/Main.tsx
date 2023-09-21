a639d507013f1cac8623c05af1aabb425afc4d2f
paskanov <v.paskanov@gmail.com>
2023-09-21 12:22:56 +0300

    Some refactoring stats

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,17 +1,21 @@
 import styled from "styled-components";
 import photo from "../../../assets/images/Ronaldo News.jpeg"
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {LastStats} from "./last-stats/LastStats";
 
 export const Main = () => {
     return (
         <StyledMain>
             <FlexWrapper align={"center"} justify={"space-around"}>
-            <div>
-                <MainTitle>FOOTBALL NEWS </MainTitle>
-            </div>
-            <Photo src={photo} alt='PSV and Philips'/>
+                <div>
+                    <MainTitle>FOOTBALL NEWS </MainTitle>
+                </div>
+                <Photo src={photo} alt='PSV and Philips'/>
+                <NewsTitle>History of partnership of PSV and Phillips</NewsTitle>
             </FlexWrapper>
+            <LastStats/>
         </StyledMain>
+
     );
 };
 
@@ -20,12 +24,16 @@ const StyledMain = styled.div`
   background-color: #f8eded;
 `
 
-
 const Photo = styled.img`
   width: 300px;
   height: 350px
 `
 
 const MainTitle = styled.h1`
-   font-size: 1.5em;
+  font-size: 1.5em;
+`
+
+const NewsTitle = styled.div `
+font-size: 1em;
+  font-weight: bold;
 `
--- src/layout/sections/main/last-stats/LastStats.tsx
+++ src/layout/sections/main/last-stats/LastStats.tsx
@@ -1,10 +1,21 @@
-
+import styled from "styled-components";
 
 export const LastStats = () => {
     return (
-        <div>
-
-        </div>
+        <StyledSkills>
+            <MainTitle>
+            LAST STATS
+            </MainTitle>
+        </StyledSkills>
     );
 };
 
+
+const StyledSkills = styled.section`
+  min-height: 100vh;
+  background-color: #ffdff9;
+`
+
+const MainTitle = styled.h1`
+  font-size: 1.5em;
+`
