19bc38e915024567a51d4fd31bd58f626aaeb374
paskanov <v.paskanov@gmail.com>
2023-09-21 12:35:20 +0300

    Some refactoring in LastStats.tsx

--- src/layout/sections/main/last-stats/LastStats.tsx
+++ src/layout/sections/main/last-stats/LastStats.tsx
@@ -1,21 +1,32 @@
 import styled from "styled-components";
+import {FlexWrapper} from "../../../../components/FlexWrapper";
+import {Icon} from "../../../../assets/icons/Icon";
 
 export const LastStats = () => {
     return (
-        <StyledSkills>
-            <MainTitle>
-            LAST STATS
-            </MainTitle>
-        </StyledSkills>
+        <StyledStats>
+            <StatsTitle>
+                <FlexWrapper>
+                    <Stat>
+                        <StatTitle>Shots</StatTitle>
+                        <StatDescription>
+                            <
+
+                        </StatDescription>
+                    </Stat>
+
+                </FlexWrapper>
+            </StatsTitle>
+        </StyledStats>
     );
 };
 
 
-const StyledSkills = styled.section`
+const StyledStats = styled.section`
   min-height: 100vh;
   background-color: #ffdff9;
 `
 
-const MainTitle = styled.h1`
+const StatsTitle = styled.h1`
   font-size: 1.5em;
 `
