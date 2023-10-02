2450c14809d8259e4b0ac6862bf5129af46caa33
paskanov <v.paskanov@gmail.com>
2023-10-02 16:33:53 +0300

    Add styled component for val players and add title

--- src/features/players/valuablePlayers/valuablePlayers.tsx
+++ src/features/players/valuablePlayers/valuablePlayers.tsx
@@ -1,10 +1,17 @@
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/StatsTitle";
 
 
 export const ValuablePlayers = () => {
     return (
-        <div>
-            MOST VALUABLE PLAYERS
-        </div>
+        <StyledValPlayers>
+            <SectionTitle> VALUABLE PLAYERS</SectionTitle>
+        </StyledValPlayers>
     );
 };
+
+const StyledValPlayers = styled.section `
+    min-height: 100vh;
+  background-color: rgba(194,231,223,0.99);
+`
 
