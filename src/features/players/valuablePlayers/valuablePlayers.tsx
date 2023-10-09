9606d76744d868d4f2dd303b9b93cc029176076c
paskanov <v.paskanov@gmail.com>
2023-10-09 16:54:20 +0300

    Add card component

--- src/features/players/valuablePlayers/valuablePlayers.tsx
+++ src/features/players/valuablePlayers/valuablePlayers.tsx
@@ -13,12 +13,12 @@ export const ValuablePlayers = () => {
             <SectionTitle> VALUABLE PLAYERS</SectionTitle>
             <Menu menuItems={region}/>
             <FlexWrapper justify={"space-around"}>
-                <Region title={region[0]} text={"European valuable players"} image={""}/>
-                <Region title={region[1]} text={"European valuable players"} image={""}/>
-                <Region title={region[2]} text={"European valuable players"} image={""}/>
-                <Region title={region[3]} text={"European valuable players"} image={""}/>
-                <Region title={region[4]} text={"European valuable players"} image={""}/>
-                <Region title={region[5]} text={"European valuable players"} image={""}/>
+                <Region title={region[0]} text={`${region[0]} valuable players`} image={""}/>
+                <Region title={region[1]} text={`${region[1]} valuable players`} image={""}/>
+                <Region title={region[2]} text={`${region[2]} valuable players`} image={""}/>
+                <Region title={region[3]} text={`${region[3]} valuable players`} image={""}/>
+                <Region title={region[4]} text={`${region[4]} valuable players`} image={""}/>
+                <Region title={region[5]} text={`${region[5]} valuable players`} image={""}/>
             </FlexWrapper>
         </StyledValPlayers>
     );
