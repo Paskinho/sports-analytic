c5f852d3651cccc8f2afb89b817d77d91c634986
paskanov <v.paskanov@gmail.com>
2023-10-09 12:42:15 +0300

    Some refactoring with menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -8,6 +8,7 @@ import {Button} from "../ui/button";
 import {Typography} from "../ui/typography";
 
 type MenuProps = {
+    name: string,
     menuItems: Array<string>
 }
 
@@ -15,40 +16,16 @@ export const Menu = (props: MenuProps) => {
     const navigate = useNavigate()
     const dispatch = useAppDispatch()
 
-    const navToHome = () => {
-        return navigate('/Main')
-    }
-
-    const navToPlayers = () => {
-        return navigate('/Players')
-    }
-
-    const navToTeams = () => {
-        return navigate('/Teams')
-    }
-
-    const navToStatistics = () => {
-        return navigate('/Statistics')
-    }
-
-    const navToSubscription = () => {
-        return navigate('/Subscription')
-    }
-
-    const navToLogin = () => {
-        return navigate('Login')
-    }
 
 
     return (
 <div>
             <StyledNav>
-                <Typography variant={'subtitle1'} color={'inherit'} onClick={navToHome}>Home</Typography>
-                <Typography variant={'subtitle1'} color={'inherit'} onClick={navToPlayers}>Players</Typography>
-                <Typography variant={'subtitle1'} color={'inherit'} onClick={navToTeams}>Teams</Typography>
-                <Typography variant={'subtitle1'} color={'inherit'} onClick={navToStatistics}>Statistics</Typography>
-                <Typography variant={'subtitle1'} color={'inherit'}
-                            onClick={navToSubscription}>Subscription</Typography>
+                <Typography variant={'subtitle1'} color={'inherit'}>{props.menuItems}</Typography>
+                <Typography variant={'subtitle1'} color={'inherit'}>{props.menuItems}</Typography>
+                <Typography variant={'subtitle1'} color={'inherit'}>{props.menuItems}</Typography>
+                <Typography variant={'subtitle1'} color={'inherit'}>{props.menuItems}</Typography>
+                <Typography variant={'subtitle1'} color={'inherit'}>{props.menuItems}</Typography>
             </StyledNav>
 
 </div>
