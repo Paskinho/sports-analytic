b0eb56dab213b60076e4ea633d3c0eb583136593
paskanov <v.paskanov@gmail.com>
2023-10-10 15:08:44 +0300

    Create story for toggle

--- src/components/ui/card/card.stories.tsx
+++ src/components/ui/card/card.stories.tsx
@@ -8,7 +8,7 @@ const meta = {
 }
 
 export default meta
-type story = StoryObj<typeof meta>
+type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
     args: {
---
+++ src/components/ui/toggle/toggle.stories.ts
@@ -0,0 +1,17 @@
+import {Toggle} from "./toggle";
+import {StoryObj} from "@storybook/react";
+
+const meta = {
+    title: 'Component/Toggle',
+    component: Toggle,
+    tags: ['autodocs']
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {
+
+    }
+}
