6bb4b1278fd167adc2b496e02398a68b7e43d04d
paskanov <v.paskanov@gmail.com>
2023-10-11 16:07:55 +0300

    Add fetch to api

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -12,12 +12,15 @@ import axios from "axios";
 
 export const instance = axios.create ({
     baseURL: 'https://api-football-v1.p.rapidapi.com/v3/',
+    // mode: "cors",
     withCredentials: true,
     headers: {
         'X-RapidAPI-Key': 'ec2759ef83msh761e98cfcedc1b3p1c651bjsn7313dedeb9af',
         'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
     }
 });
+
+fetch('api-football-v1.p.rapidapi.com', {mode: 'no-cors'}).then(response => response.json())
 
 try {
     const response = await axios.request(instance);
