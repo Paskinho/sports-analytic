539c5ac525bec7eb29d6ff5b43f2a5874892f143
paskanov <v.paskanov@gmail.com>
2023-10-12 12:20:00 +0300

    Add props to dropdown and andd library framer-motion

---
+++ src/components/ui/dropdown/dropdown.tsx
@@ -0,0 +1,42 @@
+import {CSSProperties, ReactNode} from "react";
+import { AnimatePresence, motion, MotionProps, Variants } from 'framer-motion'
+
+export type DropdownProps = {
+    /** Use TooltipItem components as children.*/
+    children: ReactNode
+    /** The preferred content alignment against the trigger. */
+    align?: 'start' | 'center' | 'end'
+    trigger?: ReactNode
+    className: string
+    style?: CSSProperties
+}
+
+const menu = {
+    closed: {
+        scale: 0,
+        transition: {
+            delay: 0.15,
+        },
+    },
+    open: {
+        scale: 1,
+        transition: {
+            type: 'spring',
+            duration: 0.4,
+            delayChildren: 0.2,
+            staggerChildren: 0.05,
+        },
+    },
+} satisfies Variants
+
+const item = {
+    variants: {
+        closed: { x: -16, opacity: 0 },
+        open: { x: 0, opacity: 1 },
+    },
+    transition: { opacity: { duration: 0.2 } },
+} satisfies MotionProps
+
+export const Dropdown = () => {
+
+}
