be8aab8593d78bacc38d9d5cea1dca1300421697
paskanov <v.paskanov@gmail.com>
2023-10-12 11:12:10 +0300

    Some changed css in profile

--- src/features/profile/personal-information.tsx
+++ src/features/profile/personal-information.tsx
@@ -1,6 +1,7 @@
 import {useAppDispatch} from "../../common/components/hooks/useAppDispatch";
 import {useNavigate} from "react-router-dom";
 import Profile from "../../assets/images/Profile.png"
+import styled from "styled-components";
 
 type PersonalInformationProps = {
     email?: string,
@@ -18,13 +19,19 @@ export const PersonalInformation = ({email, avatar, name, onLogout}: PersonalInf
     return (
         <div>Profile
             <h2>
-                <img src={Profile}/>
+                <StyledImg src={Profile}/>
             </h2>
-        <h2>name: {name}
-            {/*{!name && <TextField></TextField>}*/}
-        </h2>
-        <h2>email: {email}</h2>
+            <h2>name: {name}
+                {/*{!name && <TextField></TextField>}*/}
+            </h2>
+            <h2>email: {email}</h2>
             {avatar}
         </div>
     )
 }
+
+const StyledImg = styled.img`
+  width: 200px;
+  height: 200px;
+  border-radius: 20px;
+`
