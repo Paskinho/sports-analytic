73d57daee57aeb0c9acefa89e1606de88b9ff781
paskanov <v.paskanov@gmail.com>
2023-10-23 16:54:37 +0300

    Some refactoring

--- src/features/statistics/Statistics.tsx
+++ src/features/statistics/Statistics.tsx
@@ -1,10 +1,24 @@
 import {Checkbox} from "../../components/ui/checkbox/checkbox";
+import styled from "styled-components";
+import {Modal} from "../../components/ui/modal";
+
 
 export const Statistics = () => {
     return (
-        <div>
-  GLOBAL STATISTICS
+        <StyledDivStatistics>
+            GLOBAL STATISTICS
+            GLOBAL STATISTICS
+            GLOBAL STATISTICS
             <Checkbox>+</Checkbox>
-        </div>
+            <Modal open={true}> +++++</Modal>
+        </StyledDivStatistics>
     );
 };
+
+const StyledDivStatistics = styled.div`
+  display: flex;
+  //justify-content: center;
+  //align-items: center;
+  flex-direction: column;
+
+`
