de7cd3ed55d064eed71bc68a440371cb3007d72f
paskanov <v.paskanov@gmail.com>
2023-10-26 10:48:55 +0300

    Some refactoring table component and add scss for table

--- src/components/ui/table/table.tsx
+++ src/components/ui/table/table.tsx
@@ -1,10 +1,16 @@
 import {ComponentProps, FC} from "react";
+import {clsx} from "clsx";
+import s from "./table.module.scss"
 
 export type RootProps = ComponentProps<'table'>
 
 
-export const Root: FC<RootProps> = () => {
-    
+export const Root: FC<RootProps> = ({className,...rest}) => {
+    const classNames = {
+        table: clsx(className, s.table)
+    }
+
+    return <table className={classNames.table}/>
 }
 
 
