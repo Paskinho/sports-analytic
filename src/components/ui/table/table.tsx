672d6918490088aaddefab5f1bff107729823278
paskanov <v.paskanov@gmail.com>
2023-10-26 10:42:15 +0300

    Create table component

---
+++ src/components/ui/table/table.tsx
@@ -0,0 +1,14 @@
+import {ComponentProps, FC} from "react";
+
+export type RootProps = ComponentProps<'table'>
+
+
+export const Root: FC<RootProps> = () => {
+    
+}
+
+
+const Table = () => {
+    
+
+}
