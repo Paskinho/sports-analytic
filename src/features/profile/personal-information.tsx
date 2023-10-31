9b3f44421568f332974c9192f884bfc6312d4dcc
paskanov <v.paskanov@gmail.com>
2023-10-31 09:36:02 +0300

    Create stories for profile and some refactoring

--- src/features/profile/personal-information.tsx
+++ src/features/profile/personal-information.tsx
@@ -10,16 +10,13 @@ import Logout from "../../assets/icons/logout";
 import s from "./styles.module.scss"
 
 type PersonalInformationProps = {
-    email?: string,
+    email: string,
     avatar?: string,
-    name?: string,
+    name: string,
     onLogout?: () => void
     onAvatarChange: (newAvatar: string) => void
     onNameChange: (newName: string) => void
 }
-
-
-
 
 
 export const PersonalInformation = ({email, avatar, name, onLogout, onAvatarChange, onNameChange}: PersonalInformationProps) => {
