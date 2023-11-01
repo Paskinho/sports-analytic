6ab95c0e5822799ef4cfbf1542796733e5a37e40
paskanov <v.paskanov@gmail.com>
2023-11-01 16:11:02 +0300

    Some refactoring in virgil page

--- src/features/players/player/Liverpool/Virgil.tsx
+++ src/features/players/player/Liverpool/Virgil.tsx
@@ -3,16 +3,26 @@ import styled from "styled-components";
 import {Button} from "../../../../components/ui/button";
 
 
-export const Virgil = () => {
+type VirgilProps = {
+    name: string
+    games: number
+    nationality: string,
+    club: string,
+    age: number
+}
+
+export const Virgil = ({name, games, nationality, club, age}: VirgilProps) => {
+
     return <div className={s.page}>
         <div>#4 Virgil Van Dijk</div>
         <img className={s.img} src={"https://backend.liverpoolfc.com/sites/default/files/styles/xl/public/2023-07/virgil-van-dijk-statement-cover-.webp?itok=9Oyo-aSV"}/>
-    <form>
-        age: 31 ()
-        Nationality: Netherlands
-        Club: Liverpool
-        <Button>Watching for {"Virgil"}</Button>
-    </form>
+    <StyledFormPlayer>
+        <div>age: 31 ()</div>
+        <div>Nationality: Netherlands</div>
+        <div>  Club: Liverpool</div>
+        <div>  games: {games}</div>
+        <Button>Watching for {name}</Button>
+    </StyledFormPlayer>
     </div>
 }
 
@@ -21,3 +31,8 @@ export const Virgil = () => {
 //   height: 400px;
 //   border-radius: 20px;
 // `
+
+const StyledFormPlayer = styled.form `
+display: flex;
+  flex-direction: column;
+`
