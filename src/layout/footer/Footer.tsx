eb3b758b9e1e6e5bbf02714d43b0e0d155ecbcbd
paskanov <v.paskanov@gmail.com>
2023-11-08 12:08:58 +0300

    Some refactoring in footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,17 +1,22 @@
 import styled from "styled-components";
+import {FlexWrapper} from "../../components/FlexWrapper";
 
 
 export const Footer = () => {
     return (
         <StyledFooter>
+            <FlexWrapper direction={'column'} align={'center'}>
             <Name>Uladzislau Paskanau</Name>
             <Name>FC Liverpool</Name>
            <Copyright>Ⓒ 2023 All right reserved</Copyright>
+            </FlexWrapper>
         </StyledFooter>
     );
 };
 
 const StyledFooter = styled.footer `
+  background-color: #d8f6ef;
+  min-height: 20vh;
 `
 
 const Name = styled.span `
