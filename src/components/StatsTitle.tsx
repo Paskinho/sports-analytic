9f21d8711271a2225b6b571c7a9867f9a827f802
paskanov <v.paskanov@gmail.com>
2023-11-09 12:48:40 +0300

    Refactoring title

--- src/components/StatsTitle.tsx
+++ src/components/StatsTitle.tsx
@@ -1,7 +1,23 @@
 import styled from "styled-components";
+import {theme} from "../common/styles/Theme";
 
 export const StatsTitle = styled.h2`
   font-size: 1.5em;
   text-align: center;
+  position: relative;
+  margin-bottom: 50px;
   //font-family: 'Josefin-Sans', sans-serif;
+  
+  &::before {
+    content: "";
+    display: inline-block;
+    width: 55px;
+    height: 1px;
+    background-color: ${theme.colors.accent};
+    position: absolute;
+    left: 50%;
+    bottom: -30px;
+    transform: translateX(-50%);
+    
+  }
 `
