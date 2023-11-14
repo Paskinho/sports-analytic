77caff1bc286097e82159fe4865c1b111d7a54dc
paskanov <v.paskanov@gmail.com>
2023-11-14 12:56:45 +0300

    Some refactoring

--- src/components/StyledTable.tsx
+++ src/components/StyledTable.tsx
@@ -5,6 +5,7 @@ export const StyledTable = styled.table `
   width: 100%;
   border: none;
   margin-bottom: 20px;
+  border-collapse: separate;
 `
 
 export const StyledTd = styled.td `
@@ -14,14 +15,13 @@ export const StyledTd = styled.td `
   padding: 10px 15px;
   background: #d8d8d8;
   font-size: 14px;
-  border-left: 1px solid #ddd;
-  border-right: 1px solid #ddd;
+  border-left: 1px solid rgba(157, 231, 200, 0.99);
+  border-right: 1px solid rgba(130, 231, 169, 0.99);
 `
-
 
 export const StyledTh = styled.th `
   text-align: left;
-  border-left: 1px solid #ddd;
+  border-left: 1px solid rgb(231, 231, 231);
   border-right: 1px solid #ddd;
   padding: 10px 15px;
   font-size: 14px;
@@ -29,5 +29,8 @@ export const StyledTh = styled.th `
 `
 
 export const StyledTr = styled.tr `
+  border-left: none;
+  border-right: none;
+  background: #f3f3f3;
   //border: 5px solid black;
 `
