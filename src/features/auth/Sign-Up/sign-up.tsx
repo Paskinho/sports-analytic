cd667e6ce2636fd11d39c2853da2c4e803393a35
paskanov <v.paskanov@gmail.com>
2023-11-16 17:08:04 +0300

    Add some libraries and refactoring some code

--- src/features/auth/Sign-Up/sign-up.tsx
+++ src/features/auth/Sign-Up/sign-up.tsx
@@ -1,4 +1,8 @@
 import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import { omit } from 'remeda'
+import { DevTool } from '@hookform/devtools'
 
 const schema = z
     .object({
@@ -23,7 +27,23 @@ type SignUpProps = {
     onSubmit: (data: Omit<FormType, 'passwordConfirmation'>) => void
 }
 
-export const SignUp = () => {
+export const SignUp = (props: SignUpProps) => {
+    const {control, handleSubmit} = useForm<FormType>({
+        mode: 'oSubmit',
+        resolver: zodResolver(schema),
+        defaultValues: {
+            email: "",
+            password: "",
+            passwordConfirmation: '',
+        }
+    })
 
+    const handleFormSubmitted = handleSubmit (data => props.onSubmit(omit(data, ['passwordConfirmation'])))
+
+    return (
+        <>
+        <DevTool/>
+        </>
+    )
 
 }
