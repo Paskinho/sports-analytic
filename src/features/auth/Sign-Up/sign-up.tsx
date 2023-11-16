5d3b6441a30f26d2afef681650ce5bd3d8d8987c
paskanov <v.paskanov@gmail.com>
2023-11-16 12:49:05 +0300

    Add schema to sign-up component

---
+++ src/features/auth/Sign-Up/sign-up.tsx
@@ -0,0 +1,21 @@
+import {z} from "zod";
+
+const schema = z
+    .object({
+        email: z.string().email('Invalid email address').nonempty('Enter email'),
+        password: z.string().nonempty('Enter password'),
+        passwordConfirmation: z.string().nonempty('Confirm your password'),
+    })
+    .superRefine((data, ctx) => {
+        if (data.password !== data.passwordConfirmation) {
+            ctx.addIssue({
+                message: 'Passwords do not match',
+                code: z.ZodIssueCode.custom,
+                path: ['passwordConfirmation'],
+            })
+        }
+
+export const SignUp = () => {
+
+
+}
