5809921d096341fad46d0238ef69a6c9248e88e1
paskanov <v.paskanov@gmail.com>
2023-11-17 10:52:28 +0300

    Complete stories for signup

--- src/features/auth/Sign-Up/sign-up.stories.tsx
+++ src/features/auth/Sign-Up/sign-up.stories.tsx
@@ -1,3 +1,18 @@
-const meta = {
+import {SignUp} from "./sign-up";
+import {Meta} from "@storybook/blocks";
+import {StoryObj} from "@storybook/react";
 
+const meta = {
+    title: 'Components/SignUp',
+    component: SignUp,
+    tags: ['autodocs'],
+} satisfies Meta<typeof SignUp>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {
+        onSubmit: data => console.info(data),
+    },
 }
