06cc4a1e99cfccabbfbc9903cc5c24969f8f1130
paskanov <v.paskanov@gmail.com>
2023-11-22 12:39:46 +0300

    Some refactoring with form in new-password

--- src/features/auth/new-password/new-password.tsx
+++ src/features/auth/new-password/new-password.tsx
@@ -1,4 +1,7 @@
 import {z} from "zod";
+import {useForm} from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod'
+
 
 const schema = z.object({
     password: z.string().nonempty('Enter password'),
@@ -12,7 +15,10 @@ type NewPasswordProps = {
 }
 
 export const newPassword = (props: NewPasswordProps) => {
-
+const {conrol, handleSubmit} = useForm<FormType>({
+    mode: 'onSubmit',
+    resolver: zodResolver(schema)
+})
 
 
 
