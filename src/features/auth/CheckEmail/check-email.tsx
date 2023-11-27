3a06065756789da0b56f9dd05a0cc949f4084621
paskanov <v.paskanov@gmail.com>
2023-11-27 17:09:21 +0300

    Some refactoring with check-email

---
+++ src/features/auth/CheckEmail/check-email.tsx
@@ -0,0 +1,13 @@
+import {Card} from "../../../components/ui/card";
+
+type CheckEmailProps = {
+    email: string
+}
+
+export const CheckEmail = ({email}: CheckEmailProps) => {
+return (
+    <Card className={s.card}>
+
+    </Card>
+)
+}
