e9854f1464901b30acdb70d1e73b293f57fc8be5
paskanov <v.paskanov@gmail.com>
2023-11-29 15:47:26 +0300

    Some refactoring in teams component with table

--- src/features/teams/Teams.tsx
+++ src/features/teams/Teams.tsx
@@ -1,31 +1,46 @@
 import {Typography} from "../../components/ui/typography";
 import styled from "styled-components";
-import { Table } from '../../components/ui/table'
+import {Table} from '../../components/ui/table'
+import {useState} from "react";
+import {Card} from "../../components/ui/card";
 
 
 export const Teams = () => {
 
+    const [sort, setSort] = useState({key: "updated", direction: 'asc'})
 
-
-    const columns =[
-        {key: "Teams", sortable: true, title: "Teams"}
-
+    const columns = [
+        {key: "Teams", sortable: true, title: "Teams"},
+        {key: "updated", sortable: true, title: "Updated"},
     ]
 
     return (
         <StyledTeams>
+            <Card>
             <Typography>Teams rating on {Date()} </Typography>
-<Table.Root>
-    <Table.Header columns={columns} sort={sort}>
-        Teams
-    </Table.Header>
+            {/*<Table.Root>*/}
+            {/*    <Table.Header columns={columns} sort={sort} onSort={setSort}>*/}
+            {/*        <Table.Body>*/}
+            {/*            <Table.Row>*/}
+            {/*                <Table.Cell>*/}
 
-</Table.Root>
+            {/*                </Table.Cell>*/}
+            {/*            </Table.Row>*/}
+
+            {/*        </Table.Body>*/}
+            {/*    </Table.Header>*/}
+
+            {/*</Table.Root>*/}
+
+            <button>
+                Save Rating
+            </button>
+            </Card>
         </StyledTeams>
 
     )
 }
 
-const StyledTeams = styled.div `
-padding-top: 100px;
+const StyledTeams = styled.div`
+  padding-top: 100px;
 `
