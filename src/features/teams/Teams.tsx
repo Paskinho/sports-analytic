7fc37ebf496ad5326c69bafd3e7f9a2e39ff9c1a
paskanov <v.paskanov@gmail.com>
2023-11-29 12:57:28 +0300

    Some refactoring in teams component

--- src/features/teams/Teams.tsx
+++ src/features/teams/Teams.tsx
@@ -1,11 +1,26 @@
 import {Typography} from "../../components/ui/typography";
 import styled from "styled-components";
+import { Table } from '../../components/ui/table'
+
 
 export const Teams = () => {
+
+
+
+    const columns =[
+        {key: "Teams", sortable: true, title: "Teams"}
+
+    ]
+
     return (
         <StyledTeams>
-            <Typography>Teams rating on {Date.now()} </Typography>
+            <Typography>Teams rating on {Date()} </Typography>
+<Table.Root>
+    <Table.Header columns={columns} sort={sort}>
+        Teams
+    </Table.Header>
 
+</Table.Root>
         </StyledTeams>
 
     )
