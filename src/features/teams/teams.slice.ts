af09c46c9305192d19a9df0b6bf71581f0578557
paskanov <v.paskanov@gmail.com>
2023-11-29 16:24:31 +0300

    Some refactoring with slice of teams

--- src/features/teams/teams.slice.ts
+++ src/features/teams/teams.slice.ts
@@ -1,4 +1,5 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
+import {teamsApi} from "./teams.api"
 
 const slice = createSlice({
     name: 'teams',
@@ -8,5 +9,9 @@ const slice = createSlice({
     reducers: {}
 })
 
+const getTeamsInformation = createAsyncThunk()
 
-export const teamsReducer = slice.
+
+export const teamsReducer = slice.reducer
+export const teamsActions = slice.actions
+export const teamsThunks = {getTeamsInformation}
