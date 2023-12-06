09b980955f8a59e89aa22b0cfa4e1504ca777e06
paskanov <v.paskanov@gmail.com>
2023-12-06 09:53:25 +0300

    Create columns in cards for players

--- src/features/cards/cards.tsx
+++ src/features/cards/cards.tsx
@@ -1,6 +1,24 @@
 import s from "./cards.module.scss"
+import {z} from "zod";
+import {Column} from "../../components/ui/table";
+
+
+const newDeckSchema = z.object({
+
+})
+
 
 export const Cards = () => {
+
+    const columns: Column[] =[
+        {key: "player", sortable: true, title: "Player"},
+        {key: "country", sortable: true, title: "Country"},
+        {key: "club", sortable: true, title: "Club"},
+        {key: "age", sortable: true, title: "Age"},
+        {key: "likes", sortable: true, title: 'Likes'}
+    ]
+
+
     return (
         <div className={s.cards}>
             Cards
