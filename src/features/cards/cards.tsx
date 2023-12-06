dfe50f75577ee5b7f92bf15ff8222f76ea22a304
paskanov <v.paskanov@gmail.com>
2023-12-06 10:37:32 +0300

    Create some variables in cards component

--- src/features/cards/cards.tsx
+++ src/features/cards/cards.tsx
@@ -1,14 +1,22 @@
 import s from "./cards.module.scss"
 import {z} from "zod";
 import {Column} from "../../components/ui/table";
+import {useState} from "react";
 
 
 const newDeckSchema = z.object({
-
+    player: z.string().min(2).max(100),
+    country: z.string().min(2).max(50),
+    club: z.string().min(2).max(50),
 })
 
 
+
+type NewCard = z.infer<typeof newDeckSchema>
 export const Cards = () => {
+
+    const [sort, setSort] = useState({key:'age', direction:'asc'})
+    const sortString = sort ? `${sort.key} - ${sort.direction}` : null
 
     const columns: Column[] =[
         {key: "player", sortable: true, title: "Player"},
