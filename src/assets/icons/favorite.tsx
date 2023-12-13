1d3c556456b3ffa3674dde2d342b3e7f659b3312
paskanov <v.paskanov@gmail.com>
2023-12-13 16:59:29 +0300

    create svg component favorite (star)

---
+++ src/assets/icons/favorite.tsx
@@ -0,0 +1,12 @@
+import {SVGProps} from "react";
+
+const SvgComponent = (props: SVGProps<SVGSVGELement>, ref: Ref<SVGSVGElement>) => (
+    <svg
+    xmlns={"http://www.w3.org/2000/svg"}
+    width={15}
+    height={15}
+    >
+
+    </svg>
+
+)
