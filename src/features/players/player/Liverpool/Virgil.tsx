0207624ed0a5686a3ad91737b6a379bf8dc744dc
paskanov <v.paskanov@gmail.com>
2023-12-20 16:33:25 +0300

    Some refactoring in player component and add table

--- src/features/players/player/Liverpool/Virgil.tsx
+++ src/features/players/player/Liverpool/Virgil.tsx
@@ -18,6 +18,12 @@ type VirgilProps = {
 
 export const Virgil = ({name, games, nationality, club, age}: VirgilProps) => {
 
+    const columns = [
+        {key: 'Statistic', title: "Statistics"},
+        {key: 'Quantity', title: "Quantity"},
+        {key: 'Position in Rating', title: "Position in Rating"},
+    ]
+
     return (
         <Card className={s.page}>
 
@@ -30,14 +36,36 @@ export const Virgil = ({name, games, nationality, club, age}: VirgilProps) => {
         <Typography variant={'subtitle1'}>age: 31 ()</Typography>
         <Typography variant={'subtitle1'}>Nationality: Netherlands <img className={s.flag} alt={'Netherlands'} src={Netherlands}/></Typography>
         <Typography variant={'subtitle1'}>  Club: Liverpool <img className={s.logo} alt={Liverpool} src={Liverpool}/></Typography>
-        <Typography variant={'subtitle1'}>  games: {games}</Typography>
         <Button>Watching for Virgil {name}</Button>
-        {/*<Table.Root style={{width: '100%'}}>*/}
-        {/*    <Table.Header/>*/}
-        {/*</Table.Root>*/}
+
     </StyledFormPlayer>
         </div>
             <Typography variant={'subtitle2'}>Statistics of {name} Virgil</Typography>
+            <Table.Root style={{width: '100%'}}>
+                <Table.Header columns={columns}/>
+                <Table.Body>
+                    <Table.Row key={1}>
+                        <Table.Cell>Games</Table.Cell>
+                        <Table.Cell>23</Table.Cell>
+                        <Table.Cell>1</Table.Cell>
+                    </Table.Row>
+                    <Table.Row key={2}>
+                        <Table.Cell>Goals</Table.Cell>
+                        <Table.Cell>2</Table.Cell>
+                        <Table.Cell>8</Table.Cell>
+                    </Table.Row>
+                    <Table.Row key={3}>
+                        <Table.Cell>Assist</Table.Cell>
+                        <Table.Cell>1</Table.Cell>
+                        <Table.Cell>9</Table.Cell>
+                    </Table.Row>
+                    <Table.Row key={4}>
+                        <Table.Cell>Rating from Paskinho System</Table.Cell>
+                        <Table.Cell>7.89</Table.Cell>
+                        <Table.Cell>2</Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+            </Table.Root>
         </Card>
     )
 }
