fdc0f472819c05cbdb78abe286a4d0518a8a8303
paskanov <v.paskanov@gmail.com>
2023-12-20 15:29:46 +0300

    Some refactoring in player component and in typography

--- src/features/players/player/Liverpool/Virgil.tsx
+++ src/features/players/player/Liverpool/Virgil.tsx
@@ -22,22 +22,22 @@ export const Virgil = ({name, games, nationality, club, age}: VirgilProps) => {
         <Card className={s.page}>
 
         <div>
-        <Typography variant={'large'}>#4 Virgil Van Dijk</Typography>
+        <Typography variant={'large'}>VIRGIL VAN DIJK</Typography>
     </div>
         <div className={s.playerInfo}>
         <img className={s.img} src={"https://backend.liverpoolfc.com/sites/default/files/styles/xl/public/2023-07/virgil-van-dijk-statement-cover-.webp?itok=9Oyo-aSV"}/>
     <StyledFormPlayer>
-        <div>age: 31 ()</div>
-        <div>Nationality: Netherlands <img className={s.flag} alt={'Netherlands'} src={Netherlands}/></div>
-        <div>  Club: Liverpool <img className={s.logo} alt={Liverpool} src={Liverpool}/></div>
-        <div>  games: {games}</div>
+        <Typography variant={'subtitle1'}>age: 31 ()</Typography>
+        <Typography variant={'subtitle1'}>Nationality: Netherlands <img className={s.flag} alt={'Netherlands'} src={Netherlands}/></Typography>
+        <Typography variant={'subtitle1'}>  Club: Liverpool <img className={s.logo} alt={Liverpool} src={Liverpool}/></Typography>
+        <Typography variant={'subtitle1'}>  games: {games}</Typography>
         <Button>Watching for Virgil {name}</Button>
         {/*<Table.Root style={{width: '100%'}}>*/}
         {/*    <Table.Header/>*/}
         {/*</Table.Root>*/}
     </StyledFormPlayer>
         </div>
-
+            <Typography variant={'subtitle2'}>Statistics of {name} Virgil</Typography>
         </Card>
     )
 }
