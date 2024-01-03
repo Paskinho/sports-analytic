5040c07539da02d251b5940005ea3821cddefc49
paskanov <v.paskanov@gmail.com>
2024-01-03 12:15:39 +0300

    Some refactoring in player api component with create api

--- src/services/players/players.ts
+++ src/services/players/players.ts
@@ -1,3 +1,10 @@
-import {createApi} from "@reduxjs/toolkit/query";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query";
 
-export const playersApi = createApi
+export const playersApi = createApi({
+    reducerPath: 'playersApi',
+    tagTypes: ['Players'],
+    baseQuery: fetchBaseQuery({
+        baseUrl:import.meta.env.VITE_BASE_API_URL,
+        credentials: 'include'
+    })
+})
