73357ad24493c750978bbf115f3ec78c3bc7eca0
paskanov <v.paskanov@gmail.com>
2024-01-08 12:38:33 +0300

    Some refactoring spinner component and css

--- src/components/ui/spinner/spinner.tsx
+++ src/components/ui/spinner/spinner.tsx
@@ -1,1 +1,26 @@
+import {ComponentPropsWithoutRef, CSSProperties, ElementRef, forwardRef} from "react";
+import s from "./spinner.module.scss"
 
+
+export type SpinnerProps = {
+    fullscreen?: boolean;
+    size?: CSSProperties['width']
+} & ComponentPropsWithoutRef<'span'>>
+
+export const Spinner = forwardRef<ElementRef<'span'>, SpinnerProps>(
+    ({className, fullscreen, size = '48px', style, ...rest}, ref) => {
+    const styles = {
+        height: size,
+        width: size,
+        ...style,
+    } satisfies CSSProperties
+
+        if (fullscreen) {
+            return (
+                <div classsName={s.fullScreenContainer}>
+
+                </div>
+            )
+        }
+
+})
